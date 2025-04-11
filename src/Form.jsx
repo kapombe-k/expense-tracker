f03977b9babf1bb55a1f927a3351b6613ab1e9ac
Eugene Kapombe <kapombek@gmail.com>
2025-04-11 12:53:26 +0300

    populated divs

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,9 +1,14 @@
 export function Table() {
     return (
-        <div>
-            <form>
-                
-            </form>
-        </div>
+      <div>
+        <h3>Add expense</h3>
+        <form>
+          <input input type="text"/></input>
+          <input input type="text"></input>
+          <input input type="text"></input>
+          <input type="number"></input>
+          <input type="date"></input>
+        </form>
+      </div>
     );
  };
